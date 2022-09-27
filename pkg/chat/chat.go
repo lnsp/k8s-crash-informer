71a312b6810cc688ddd1eac056bd538c57e7c184
Lennart Espe <3391295+lnsp@users.noreply.github.com>
2022-09-27 18:24:01 +0200

    Add support for Telegram in chat package

--- pkg/chat/chat.go
+++ pkg/chat/chat.go
@@ -2,7 +2,10 @@ package chat
 
 import (
 	"fmt"
+	"strings"
+	"unicode/utf16"
 
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/mattermost/mattermost-server/v6/model"
 	"github.com/slack-go/slack"
@@ -84,6 +87,8 @@ func NewClientFromEnv() (Client, error) {
 		client, err = NewMattermostClientFromEnv()
 	case "slack":
 		client, err = NewSlackClientFromEnv()
+	case "telegram":
+		client, err = NewTelegramClientFromEnv()
 	default:
 		err = fmt.Errorf("unknown client type: %s", cfg.Type)
 	}
@@ -161,5 +166,82 @@ func NewSlackClientFromEnv() (*SlackClient, error) {
 	return &SlackClient{
 		Client:  api,
 		Channel: cfg.Channel,
+	}, nil
+}
+
+type TelegramConfig struct {
+	Token  string
+	ChatID int64
+}
+
+type TelegramClient struct {
+	chat tgbotapi.Chat
+	bot  *tgbotapi.BotAPI
+}
+
+func (client *TelegramClient) Send(note *CrashNotification) {
+	// Generate text
+	type entityMarker struct {
+		Type    string
+		Content string
+	}
+	contents := []entityMarker{
+		{"bold", note.Title},
+		{"", note.Message},
+		{"", "\n"},
+		{"bold", "Logs"},
+		{"pre", note.Logs},
+		{"", "\n"},
+		{"bold", "Reason"},
+		{"pre", note.Reason},
+	}
+	// Generate text
+	cleartext := []string{}
+	for _, c := range contents {
+		cleartext = append(cleartext, c.Content)
+	}
+	// Get all joined groups
+	message := tgbotapi.NewMessage(client.chat.ID, strings.Join(cleartext, "\n"))
+	offset := 0
+	// Generate list of entities
+	for _, c := range contents {
+		length := len(utf16.Encode([]rune(c.Content)))
+		if c.Type != "" {
+			message.Entities = append(message.Entities, tgbotapi.MessageEntity{
+				Type:   c.Type,
+				Offset: offset,
+				Length: length,
+			})
+		}
+		offset += length + 1
+	}
+	_, err := client.bot.Send(message)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+// NewTelegramClientFromEnv instantiates and configures a Telegram client.
+func NewTelegramClientFromEnv() (*TelegramClient, error) {
+	var cfg TelegramConfig
+	if err := envconfig.Process("telegram", &cfg); err != nil {
+		return nil, err
+	}
+	bot, err := tgbotapi.NewBotAPI(cfg.Token)
+	if err != nil {
+		return nil, fmt.Errorf("init bot: %w", err)
+	}
+	// Get chat to verify
+	chat, err := bot.GetChat(tgbotapi.ChatInfoConfig{
+		ChatConfig: tgbotapi.ChatConfig{
+			ChatID: cfg.ChatID,
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("get chat: %w", err)
+	}
+	return &TelegramClient{
+		chat: chat,
+		bot:  bot,
 	}, nil
 }
