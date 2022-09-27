bfb665ab28a4fb39603c79fcf389ea60ff0dcb81
Lennart Espe <3391295+lnsp@users.noreply.github.com>
2022-09-27 19:38:49 +0200

    Replace ioutil.ReadFile with os.ReadFile

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -2,14 +2,14 @@ package utils
 
 import (
 	"fmt"
-	"io/ioutil"
+	"os"
 )
 
 const namespaceFilePath = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
 
 // Namespace returns the namespace this pod is running in.
 func Namespace() (string, error) {
-	nsfile, err := ioutil.ReadFile(namespaceFilePath)
+	nsfile, err := os.ReadFile(namespaceFilePath)
 	if err != nil {
 		return "", fmt.Errorf("could not read namespace: %v", err)
 	}
